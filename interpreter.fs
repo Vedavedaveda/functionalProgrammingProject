c634bb7e9a60c66e06136c1122ee65a9eaf16fa6
vedavedaveda <veda@ruc.dk>
2024-04-29 11:54:41 +0200

    interpreter updates

--- interpreter.fs
+++ interpreter.fs
@@ -1,29 +1,65 @@
+module Interpreter
+type exp =
+    | INT of int
+    | VAR of string
+    | BOOL of bool
+    | ADD of exp * exp
+    | SUB of exp * exp
+    | MUL of exp * exp
+    | DIV of exp * exp
+    | MOD of exp * exp
+    | EQ of exp * exp
+    | NEQ of exp * exp
+    | LT of exp * exp
+    | LTE of exp * exp
+    | GT of exp * exp
+    | GTE of exp * exp
+    | AND of exp * exp
+    | OR of exp * exp
+    | IF of exp * exp * exp
 
-type ’a env = (varname * ’a) list
-let rec lookup x = function
-    | [] -> failwith ("unbound: " + x)
-    | (y, w) :: env -> if x = y then w else lookup x env
+let isInteger = function
+    | INT _ -> true
+    | _ -> false
 
-// eval : int ienv -> exp -> int
+let isBoolean = function
+    | BOOL _ -> true
+    | _ -> false
+
 let evalProg (funcs, e) = 
     let rec eval env = function
-        //
-        | Syntax.INT i           -> i 
-        //
-        | Syntax.VAR x           -> lookup x env 
-        //
-        | Syntax.ADD (e1, e2)    -> let v1 = eval env e1
-                                    let v2 = eval env e2
-                                    v1 + v2
-        //
-        | Syntax.EQ (e1, e2)     -> let v1 = eval env e1
-                                    let v2 = eval env e2
-                                    if v1 = v2 then 1 else 0
-        //
-        | Syntax.CALL (f, e)     -> let v = eval env e
-                                    let (x, body) = lookup f funcs
-                                    eval [(x, v)] body
-        //
-        | Syntax.LET (x, e1, e2) -> let v1 = eval env e1
-                                    eval((x,v1) :: env) e2
-eval [] e
+        | INT i -> i
+        | BOOL b -> if b then 1 else 0
+        | ADD (e1, e2) ->  
+                        if not (isInteger e1 && isInteger e2) then
+                            failwith "Both expressions must be integers."
+                        let v1 = eval env e1
+                        let v2 = eval env e2
+                        v1 + v2
+        | SUB (e1, e2) ->  
+                        if not (isInteger e1 && isInteger e2) then
+                            failwith "Both expressions must be integers."
+                        let v1 = eval env e1
+                        let v2 = eval env e2
+                        v1 - v2
+        | EQ (e1, e2) ->  
+                        if not (isInteger e1 && isInteger e2) then
+                            failwith "Both expressions must be integers."
+                        let v1 = eval env e1
+                        let v2 = eval env e2
+                        if v1 = v2 then 1 else 0
+        | IF (e1, e2, e3) ->
+                            if not (isBoolean e1) then
+                                failwith "Condition must be a boolean."
+                            let v1 = eval env e1
+                            let v2 = eval env e2
+                            let v3 = eval env e3
+                            if v1 = 1 then v2 else v3
+
+        | BOOL b -> if b then 1 else 0 // Evaluation of boolean literals
+        | VAR s -> failwith "Variable evaluation not implemented"
+        | _ -> failwith "Not implemented"  // Add other cases as needed
+    eval [] e
+open Interpreter
+let result = evalProg ([], IF (BOOL false, INT 1, INT 5));;
+// #load "interpreter.fs";;
